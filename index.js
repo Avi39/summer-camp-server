e9c5b977d7212f9f6ca67cfe380f843c8f70b9f9
Avi <avishekuser@gmai.com>
2023-06-15 22:15:39 +0600

    users get api updated

--- index.js
+++ index.js
@@ -29,8 +29,21 @@ async function run() {
     const cartCollection = client.db("marshalDb").collection("carts");
 
     // users related apis
+
+    app.get('/users',async (req,res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users',async(req,res)=>{
       const user = req.body;
+      console.log(user);
+      const query = {email:user.email};
+      const existingUser = await usersCollection.findOne(query);
+      console.log('existing user',existingUser);
+      if(existingUser){
+        return res.send({message: 'user already exist'});
+      }
       const result =await usersCollection.insertOne(user);
       res.send(result);
     })
