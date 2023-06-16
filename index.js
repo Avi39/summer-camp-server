814d38b06f76bf9a029e55a6a2581f19d4346e98
Avi <avishekuser@gmai.com>
2023-06-16 20:30:23 +0600

    approve status check api done

--- index.js
+++ index.js
@@ -95,6 +95,24 @@ async function run() {
       res.send(result);
     })
 
+    // check active status by admin
+    app.patch('/users/adminApprove/:id',async (req,res)=>{
+      const id = req.params.id;
+      // console.log(id);
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          status: 'active'
+        },
+      };
+      const result = await classesCollection.updateOne(filter,updateDoc);
+      res.send(result);
+    })
+
+    // check denied status by admin
+    
+
+
     // check instructor
      app.get('/users/instructor/:email',verifyJWT,async(req,res)=>{
       const email = req.params.email;
